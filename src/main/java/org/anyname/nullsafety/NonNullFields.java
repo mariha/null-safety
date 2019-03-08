3a551c9e7d4d1793226bfe2f2d5c9003706a63c2
Mariha Kozinska <m.kozinska@gmail.com>
2019-03-08 17:11:07 -0600

    Fix missing meta-annotations for @NonNullFields

--- src/main/java/org/anyname/nullsafety/NonNullFields.java
+++ src/main/java/org/anyname/nullsafety/NonNullFields.java
@@ -1,5 +1,13 @@
 package org.anyname.nullsafety;
 
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Declares that fields in an annotated scope are non-null unless otherwise explicitly specified as {@link Nullable}.
  * Annotated scope can be package (in package-info.java), a class or enum type. Inner and nested classes are in scope of
@@ -7,5 +15,10 @@ package org.anyname.nullsafety;
  *
  * @see org.anyname.nullsafety
  */
+@Target({ElementType.PACKAGE, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Nonnull
+@TypeQualifierDefault(ElementType.FIELD)
 public @interface NonNullFields {
 }
